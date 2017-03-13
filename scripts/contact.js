de55221e9b8bc378e634e714be3e169ea69ff34f
Melissa Buena <Mbuena@student.neumont.edu>
2017-03-13 17:46:27 -0600

    Footer always bottom, Contact Page Effects Done

--- scripts/contact.js
+++ scripts/contact.js
@@ -4,49 +4,57 @@ var newSize1;
 var oldsize2;
 var newSize2;
 $(document).ready(function() {
+
+    var zoom1 = false;
     oldSize1=parseFloat($("#enlarge1").css('font-size'));
     newSize1=oldSize1  * 2;
-  $("#enlarge1").hover(
-    function() {
-     $("#enlarge1").animate({ fontSize: newSize1}, 200);
+    $('#enlarge1').on('click',function(){
+       if(zoom1){
+ $("#enlarge1").animate({ fontSize: newSize1}, 200);
      $(this).css('color', '#FF8C1D');
     $(this).css('-webkit-transition', '.35s ease-out');
-
-    },
-    function() {
-    $("#enlarge1").animate({ fontSize: oldSize1}, 200);
+           zoom1=false;
+       }
+        else{
+      $("#enlarge1").animate({ fontSize: oldSize1}, 200);
     $(this).css('color', 'black');
-   }
- );
+           zoom1=true;
+       } 
+    });
     
+    
+        var zoom2 = false;
     oldSize2=parseFloat($("#enlarge2").css('font-size'));
     newSize2=oldSize2  * 2;
-  $("#enlarge2").hover(
-    function() {
-     $("#enlarge2").animate({ fontSize: newSize2}, 200);
+    $('#enlarge2').on('click',function(){
+       if(zoom2){
+ $("#enlarge2").animate({ fontSize: newSize2}, 200);
      $(this).css('color', '#FF8C1D');
-     $(this).css('-webkit-transition', '.35s ease-out');
-    },
-    function() {
-    $("#enlarge2").animate({ fontSize: oldSize2}, 200);
+    $(this).css('-webkit-transition', '.35s ease-out');
+           zoom2=false;
+       }
+        else{
+      $("#enlarge2").animate({ fontSize: oldSize2}, 200);
     $(this).css('color', 'black');
-   }
- );
+           zoom2=true;
+       } 
+    });
     
+        var zoom3 = false;
     oldSize3=parseFloat($("#enlarge3").css('font-size'));
     newSize3=oldSize3  * 2;
-  $("#enlarge3").hover(
-    function() {
-     $("#enlarge3").animate({ fontSize: newSize3}, 200);
+    $('#enlarge3').on('click',function(){
+       if(zoom3){
+ $("#enlarge3").animate({ fontSize: newSize3}, 200);
      $(this).css('color', '#FF8C1D');
-     $(this).css('-webkit-transition', '.35s ease-out');
-    },
-    function() {
-    $("#enlarge3").animate({ fontSize: oldSize3}, 200);
+    $(this).css('-webkit-transition', '.35s ease-out');
+           zoom3=false;
+       }
+        else{
+      $("#enlarge3").animate({ fontSize: oldSize3}, 200);
     $(this).css('color', 'black');
-   }
- );
-
-
+           zoom3=true;
+       } 
+    });
 
 });
