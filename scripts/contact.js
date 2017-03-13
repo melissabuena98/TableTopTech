aae427761690f50bc3edfca062cee2dadcd824de
Melissa Buena <Mbuena@student.neumont.edu>
2017-03-13 11:58:50 -0600

    Progress on Contact Us

---
+++ scripts/contact.js
@@ -0,0 +1,7 @@
+
+    $('#enlarge').on('mouseover', function(){
+        $(this).fontSize('150px');
+    });
+       $('#enlarge').on('mouseout', function(){
+      $(this).fontSize('16px'); 
+    });
