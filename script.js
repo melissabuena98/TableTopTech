4871421de580c627fb50ae3d4757059d971e45d6
Caleb Carlile <ccarlile@student.neumont.edu>
2017-03-12 20:48:44 -0600

    SlideShow

--- script.js
+++ script.js
@@ -2,3 +2,39 @@
 //var ctx = c.getContext("2d");
 //
 //c.style.backgroundColor="white";
+var slideIndex = 1;
+var idle = false;
+setInterval(cycle, 5000);
+
+showSlides(slideIndex);
+
+function cycle() {
+    showSlides(slideIndex += 1);
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i, dots = document.getElementsByClassName("dot"), slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
